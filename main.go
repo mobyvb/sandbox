2d9713423539c3919765582b8543f475401a2502
mobyvb <mobyvb@gmail.com>
2023-04-22 22:39:35 -0400

    Update files
    I added a quote from Sam Gamgee to the README.md file.
    I added an endpoint at /api/number to the main.go file to return a random number between 20 and 50.
    I modified the index.html file to have a different color for the content section compared to the background.
    
    Resolves: #5

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/api/number", func(w http.ResponseWriter, r *http.Request) {
+		randomNumber := rand.Intn(31) + 20
+		fmt.Fprint(w, strconv.Itoa(randomNumber))
 	})
 
 	fmt.Println("Server starting on port 8080...")
